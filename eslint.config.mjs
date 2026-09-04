1f710aa87998c151f7cdc8e3e4eac9ec853c15f0
teslo <teslo.knife@gmail.com>
2026-09-04 18:18:09 +0500

    Настройка eslint без prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+// @ts-nocheck
+import expoConfig from "eslint-config-expo/flat";
+import reactHooks from "eslint-plugin-react-hooks";
+import { defineConfig } from "eslint/config";
+import ts from "typescript-eslint";
+
+module.exports = defineConfig([
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/.expo/**",
+      "**/dist/**",
+      "**/.next/**",
+      "**/.android/**",
+      "**/ios/**",
+      "**/expo-env.d.ts",
+    ],
+  },
+
+  expoConfig,
+  ...ts.configs.recommended,
+  reactHooks.configs.flat.recommended,
+
+  {
+    files: ["**/*.{ts,tsx}"],
+    rules: {
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          prefer: "type-imports",
+          fixStyle: "inline-type-imports",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "error",
+      "no-console": [
+        "warn",
+        {
+          allow: ["warn", "error"],
+        },
+      ],
+    },
+  },
+]);
